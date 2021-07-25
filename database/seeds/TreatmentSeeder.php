e748b3bce43743b9c75e8200c2d3c1df10e74cad
Frank Josueé Vigil Vega <frankjosue.vigilvega@gmail.com>
2021-07-25 22:31:16 +0200

    agreagando las frases de los primeras voces

--- database/seeds/TreatmentSeeder.php
+++ database/seeds/TreatmentSeeder.php
@@ -13,18 +13,19 @@ class TreatmentSeeder extends Seeder
     public function run()
     {
         //
-        // $faker = Faker::create();
-        // for ($i=0; $i < 10; $i++) {
-        //     \DB::table("treatments")->insert(
-        //           array(
-        //                 //'id'         => $faker->unique()->randomNumber,
-        //                 'name'          => $faker->firstName("male"),
-        //                 'desc'          => $faker->sentence,  
-        //                 'specialist_id' => 1,                        
-        //                 'created_at'    => date('Y-m-d H:m:s'),
-        //                 'updated_at'    => date('Y-m-d H:m:s')
-        //           )
-        //     );
-        // }
+         $faker = Faker::create();
+         for ($i=0; $i < 1; $i++) {
+             \DB::table("treatments")->insert(
+                   array(
+                         //'id'         => $faker->unique()->randomNumber,
+                         'name'          => "Frases Neurologia",
+                         'desc'          => "HAciendo banco de voces para el trabajo de Interligibilidad", 
+                         'status'        => 1,
+                         'specialist_id' => 2,                        
+                         'created_at'    => date('Y-m-d H:m:s'),
+                         'updated_at'    => date('Y-m-d H:m:s')
+                   )
+             );
+         }
     }
 }
